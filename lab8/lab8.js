627455b06da77030edc18d3b7950ce5fecd11718
Софья Свиридова <idv251274@gmail.com>
2024-06-02 16:06:35 +0700

    День недели

--- lab8/lab8.js
+++ lab8/lab8.js
@@ -31,3 +31,15 @@ function showDate() {
         День недели: ${daysOfWeek[today.getDay()]}
     `;
 }
+
+function showDayOfWeek() {
+    let day = document.getElementById('input-day').value;
+    let month = document.getElementById('input-month').value - 1;
+    let year = document.getElementById('input-year').value;
+    let date = new Date(year, month, day);
+    let daysOfWeek = ['воскресенье', 'понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота'];
+    let dayOfWeek = daysOfWeek[date.getDay()];
+    let resultDiv = document.getElementById('day-of-week-result');
+    resultDiv.innerHTML = `День недели: ${dayOfWeek}`;
+}
+
