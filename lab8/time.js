98660273b7a661372e9f6701ec6c716af9548fe7
Софья Свиридова <idv251274@gmail.com>
2024-06-02 17:26:54 +0700

    Аналоговые часы

--- lab8/time.js
+++ lab8/time.js
@@ -6,3 +6,15 @@ function showTime() {
     document.getElementById('seconds').innerHTML = sec;
 }
 setInterval(showTime, 1000);
+
+let totalDegrees = 0;
+
+function showhand() {
+    let secondHand = document.getElementById('second-hand');
+    let now = new Date();
+    let seconds = now.getSeconds();
+    totalDegrees = seconds * 6;  // 6 градусов каждую секунду
+    secondHand.style.transform = `rotate(${totalDegrees}deg)`;
+}
+
+setInterval(showhand, 1000);
